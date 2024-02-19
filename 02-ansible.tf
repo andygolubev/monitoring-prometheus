fcfdc3d24260935510aef4b8ea3b19ba07a409c3
Andy Golubev <30592974+andygolubev@users.noreply.github.com>
2024-02-19 16:57:20 +0200

    fix pt repo

--- 02-ansible.tf
+++ 02-ansible.tf
@@ -2,6 +2,6 @@ resource "ansible_playbook" "playbook" {
   playbook   = "playbook-monitoring-setup.yml"
   name       = digitalocean_droplet.monitoring.ipv4_address
   replayable = true
-#   verbosity  = 6
+  verbosity  = 3
   depends_on = [ null_resource.addkey ]
 }
