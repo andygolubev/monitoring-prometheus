9ca5250e60c0984c2e3e8bb953e68534a72d6f1e
Andy Golubev <30592974+andygolubev@users.noreply.github.com>
2024-02-19 17:49:07 +0200

    disable ansible provider

--- 02-ansible.tf
+++ 02-ansible.tf
@@ -1,7 +1,24 @@
-resource "ansible_playbook" "playbook" {
-  playbook   = "playbook-monitoring-setup.yml"
-  name       = digitalocean_droplet.monitoring.ipv4_address
-  replayable = true
-  verbosity  = 3
+# resource "ansible_playbook" "playbook" {
+#   playbook   = "playbook-monitoring-setup.yml"
+#   name       = digitalocean_droplet.monitoring.ipv4_address
+#   replayable = true
+#   verbosity  = 3
+#   depends_on = [ null_resource.addkey ]
+# }
+
+
+resource "null_resource" "make_inventory" {
+  provisioner "local-exec" {
+    command = "echo ${digitalocean_droplet.monitoring.ipv4_address}) > ./inventory.ini"
+  }
+
   depends_on = [ null_resource.addkey ]
+}
+
+resource "null_resource" "run_playbook" {
+  provisioner "local-exec" {
+    command = "ansible-playbook -i ./inventory.ini ./playbook-monitoring-setup.yml"
+  }
+
+  depends_on = [ null_resource.make_inventory ]
 }
