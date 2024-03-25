a7d55964d2375c6080e53bed41e5bd6b7b90ac49
Andy Golubev <30592974+andygolubev@users.noreply.github.com>
2024-03-25 19:17:00 +0200

    make role

--- 02-dns-records.tf
+++ 02-dns-records.tf
@@ -5,7 +5,7 @@ data "digitalocean_domain" "this" {
 resource "digitalocean_record" "monitoring-dns-record" {
   domain = data.digitalocean_domain.this.id
   type   = "A"
-  name   = "monitoring-test"
+  name   = "monitoring-test2"
   value  = digitalocean_droplet.monitoring.ipv4_address
 
   depends_on = [ digitalocean_droplet.monitoring ]
