8b229725f9fe6c13ba2913ffb93e104815aab641
Andy Golubev <30592974+andygolubev@users.noreply.github.com>
2024-03-26 10:49:00 +0200

    add template

--- 02-dns-records.tf
+++ 02-dns-records.tf
@@ -10,3 +10,13 @@ resource "digitalocean_record" "monitoring-dns-record" {
 
   depends_on = [ digitalocean_droplet.monitoring ]
 }
+
+resource "digitalocean_record" "vault-dns-record" {
+  domain = data.digitalocean_domain.this.id
+  type   = "A"
+  name   = "vault-test2"
+  value  = digitalocean_droplet.monitoring.ipv4_address
+
+  depends_on = [ digitalocean_droplet.monitoring ]
+}
+
