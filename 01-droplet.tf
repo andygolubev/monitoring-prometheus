6a7a91515385f74558a6b4f51f9cff3de3b1ee0c
Andy Golubev <30592974+andygolubev@users.noreply.github.com>
2024-03-31 19:48:24 +0300

    fix

--- 01-droplet.tf
+++ 01-droplet.tf
@@ -7,11 +7,13 @@ resource "digitalocean_ssh_key" "devopsvm" {
 # Create a new Droplet using the SSH key
 resource "digitalocean_droplet" "monitoring" {
   image  = "ubuntu-23-10-x64"
-  name   = "monitoring_vault"
+  name   = "monitoring-vault"
   region = "fra1"
   size   = "s-1vcpu-1gb"
   #size   = "s-1vcpu-2gb"
   ssh_keys = [digitalocean_ssh_key.devopsvm.fingerprint]
+
+  tags   = ["Monitoring", "Vault"]
 
   provisioner "local-exec" {
     command = "echo ${digitalocean_droplet.monitoring.ipv4_address} > ./playbook_inventory"
